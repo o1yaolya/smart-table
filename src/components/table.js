9f1defaeee26e506189166f72ab893a02d665034
Olya Kozlova <o1yaolya@yandex.ru>
2025-09-29 01:51:37 +0300

    1

--- src/components/table.js
+++ src/components/table.js
@@ -12,12 +12,34 @@ export function initTable(settings, onAction) {
     const root = cloneTemplate(tableTemplate);
 
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(subName => {               // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName);             // клонируем и получаем объект, сохраняем в таблице
+    root.container.prepend(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+});
+
+    after.forEach(subName => {                         // перебираем нужный массив идентификаторов
+    root[subName] = cloneTemplate(subName);            // клонируем и получаем объект, сохраняем в таблице
+    root.container.append(root[subName].container);    // добавляем к таблице после (append) или до (prepend)
+});
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
+        const nextRows = data.map(item => {
+       const row = cloneTemplate(rowTemplate);
+       Object.keys(item).forEach(key => {
+        // Проверяем, существует ли ключ в элементах шаблона
+        
+            if (row.elements[key]) {
+                // Присваиваем значение соответствующему элементу
+                row.elements[key].textContent = item[key];
+                row.container.appendChild(row.elements[key]);
+                }
+            })
+            return row.container;
+            })
+
         root.elements.rows.replaceChildren(...nextRows);
     }
 
